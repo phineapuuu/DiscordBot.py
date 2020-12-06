9a68d6076d62737543b3289b0309992c6b9ab8ae
Ricky Rodrigo <75511393+Riiickyy@users.noreply.github.com>
2020-12-06 09:25:39 +0800

    Add existing work from Archion

---
+++ constants/strings.py
@@ -0,0 +1,17 @@
+  
+YESNO = {
+    'y': "confirmed",
+    'n': "cancelled",
+    't': "timed out",
+}
+
+EMPTY_LIST = "(none)"
+
+CONTINUED = " _cont._"
+
+TIME_FORMAT = 'UTC %H:%M:%S on %Y-%m-%d'
+
+CONFIRM_COMMANDS = ('!confirm', '!y', '!yes')
+CANCEL_COMMANDS = ('!cancel', '!n', '!no')
+CONFIRM_MSGS = CONFIRM_COMMANDS + ('y', 'yes')
+CANCEL_MSGS = CANCEL_COMMANDS + ('n', 'no')
