c5cee94a0f52256c7e593f081faee6a2f912ad4c
Ricky Rodrigo <75511393+Riiickyy@users.noreply.github.com>
2020-12-06 09:24:41 +0800

    Add existing work from Archion

---
+++ constants/colors.py
@@ -0,0 +1,47 @@
+BLACK           = 0x000000
+GREY1           = 0x333333
+GREY2           = 0x666666
+GREY3           = 0x999999
+GREY4           = 0xCCCCCC
+WHITE           = 0xFFFFFF
+
+RED             = 0xFF0000
+YELLOW          = 0xFFFF00
+GREEN           = 0x00FF00
+CYAN            = 0x00FFFF
+BLUE            = 0x0000FF
+MAGENTA         = 0xFF00FF
+
+SOFT_RED        = 0x993333
+SOFT_GREEN      = 0x339933
+SOFT_BLUE       = 0x6666FF
+
+ORANGE          = 0xFF9900
+PURPLE          = 0x7F007F
+
+BLURPLE         = 0x7289DA
+GREYPLE         = 0x99AAB5
+DARK            = 0x2C2F33
+NOT_QUITE_BLACK = 0x23272A
+
+NONE            = 0x4F545C
+
+ASK             = CYAN
+CONFIRM         = SOFT_GREEN
+CANCEL          = SOFT_RED
+TIMEOUT         = NONE
+
+DELETED         = NONE
+TEMPORARY       = NONE
+
+INFO            = BLURPLE
+HELP            = PURPLE
+ERROR           = RED
+SUCCESS         = GREEN
+LINK            = SOFT_BLUE
+
+YESNO           = {
+    'y': CONFIRM,
+    'n': CANCEL,
+    't': TIMEOUT,
+}
